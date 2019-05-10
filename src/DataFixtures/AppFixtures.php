4cb2126f9206fa94109c2842399647a4fa474c64
liloo2040 <e.dufour.89@gmail.com>
2019-05-10 18:43:28 +0200

    working with faker and slugify

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,21 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ad;
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use App\Entity\Ad;
+use Cocur\Slugify\Slugify;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('fr-FR');
+        $slugify = new Slugify();
+
         for ($i = 1; $i <= 30; $i++) {
             $ad = new Ad();
-            $ad->setTitle("Titre de l'annonce n°$i")
-                ->setSlug("titre-de-l-annonce-n-$i")
-                ->setCoverImage("http://placehold.it/1000x300")
-                ->setIntroduction("Voici une introduction")
-                ->setContent("<p>Voici un contenu.</p>")
+            $title = $faker->sentence();
+            $slug = $slugify->slugify($title);
+            $coverImage = $faker->imageUrl(1000, 350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+            $ad->setTitle($title)
+                ->setSlug($slug)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
                 ->setRooms(mt_rand(1, 5));
 
