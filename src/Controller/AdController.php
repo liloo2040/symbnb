d86beda88de699ff6eb6e510b682691e07bc0942
liloo2040 <e.dufour.89@gmail.com>
2019-05-13 18:11:14 +0200

    upgrading base.html.twig and adding custom css

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,12 +13,25 @@ class AdController extends Controller
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo)
     {
-        $repo = $this->getDoctrine()->getRepository(Ad::class);
+
         $ads = $repo->findAll();
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
         ]);
+    }
+
+    /**
+     * @Route("/ads/{slug}", name="ads_show")
+     *
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo)
+    {
+        $ad = $repo->findOneBySlug($slug);
+        return $this->render('ad/show.html.twig', array(
+            'ad' => $ad
+        ));
     }
 }
