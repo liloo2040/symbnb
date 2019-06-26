182ebfbc865fdcb35b35ad1f880b45253b87e9ca
liloo2040 <e.dufour.89@gmail.com>
2019-06-26 18:02:29 +0200

    adding registration form

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AccountController extends AbstractController
+{
+    /**
+     * Connexion
+     * @Route("/login", name="account_login")
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig', array(
+            'hasError' => $error !== null,
+            'username' => $username
+        ));
+    }
+
+    /**
+     * Déconnexion
+     *
+     * @Route("/logout", name="account_logout")
+     * @return void
+     */
+    public function logout()
+    { }
+
+    /**
+     * Affichage du formulaire d'inscription
+     *
+     * @Route("/register", name="account_register")
+     * @return Response
+     */
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getHash());
+            $user->setHash($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été créé"
+            );
+
+            return  $this->redirectToRoute('account_login');
+        }
+
+        return $this->render('account/registration.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
