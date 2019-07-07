00da5ffc748c77b77f17952427e65a78e259f3ec
liloo2040 <e.dufour.89@gmail.com>
2019-07-07 18:47:15 +0200

    adding confirm password form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -12,6 +12,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Form\AccountType;
+use App\Entity\PasswordUpdate;
+use App\Form\PasswordUpdateType;
+use Symfony\Component\Form\FormError;
 
 class AccountController extends AbstractController
 {
@@ -98,6 +101,48 @@ class AccountController extends AbstractController
             );
         }
         return $this->render('account/profile.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * Permet de modifier le mot de passe
+     *
+     * @Route("/account/password-update", name="account_password")
+     * @return Response
+     */
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder, ObjectManager $manager)
+    {
+        $passwordUpdate = new PasswordUpdate();
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //1. Vérifier que le oldPassword soit le même que celui de l'utilisateur
+            if (!password_verify($passwordUpdate->getOldPassword(), $user->getHash())) {
+                //Gérer l'erreur
+                $form->get('oldPassword')->addError(new FormError("Le mot de passe que vous avez saisi n'est pas votre mot de passe actuel."));
+            } else {
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+
+                $user->setHash($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe a bien été modifié."
+                );
+                return $this->redirectToRoute('');
+            }
+        }
+        return $this->render('account/password.html.twig', array(
             'form' => $form->createView()
         ));
     }
