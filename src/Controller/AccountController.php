9295aee0302b74352eed2359017d20233ebf1c3b
liloo2040 <e.dufour.89@gmail.com>
2024-04-28 16:33:07 +0200

    upgrade 5.4

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\Security as SecurityCore;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -83,7 +83,7 @@ class AccountController extends AbstractController
      * Affiche le formulaire de modification de profil
      *
      * @Route("/account/profile", name="account_profile")
-     * @IsGranted("ROLE_USER")
+     * @Security("is_granted('ROLE_USER')")
      * 
      * @return Response
      */
@@ -113,7 +113,7 @@ class AccountController extends AbstractController
      * Permet de modifier le mot de passe
      *
      * @Route("/account/password-update", name="account_password")
-     * @IsGranted("ROLE_USER")
+     * @Security("is_granted('ROLE_USER')")
      * @return Response
      */
     public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder, EntityManagerInterface $manager)
@@ -156,7 +156,7 @@ class AccountController extends AbstractController
      * Affichage profil
      *
      * @Route("/account", name="account_index")
-     * @IsGranted("ROLE_USER")
+     * @Security("is_granted('ROLE_USER')")
      * @return Response
      */
     public function myAccount()
